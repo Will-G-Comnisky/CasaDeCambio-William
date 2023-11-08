4f82c39020e5411bf0ccd3154d58626ec75aab48
William <william.gispiela@gmail.com>
2023-11-08 15:35:11 -0300

    separação e criação de arquivos, diferenciado ajax e fetch, e praticando funções de callback em mainAJAX

--- mainFETCH.js
+++ mainFETCH.js
@@ -86,37 +86,3 @@ btnConverter.addEventListener('click', async function() {
 });
 
 
-
-
-
-/*
-
----------------ANOTAÇÕES DE COMO SERIA COM AJAX------------------------
-
-function buscarMoedas() {
-    var xhr = new XMLHttpRequest();
-    
-    xhr.open('GET', 'https://olinda.bcb.gov.br/olinda/servico/PTAX/versao/v1/odata/Moedas?$top=100&$format=json&$select=simbolo,nomeFormatado')
-    
-    xhr.addEventListener('load', function() {
-        let moedas = JSON.parse(xhr.responseText);
-        console.log(moedas.value[1]);
-        
-        moedas.value.forEach(moeda => {
-            let option = document.createElement('option');
-            option.value = moeda.simbolo + ' (' + moeda.nomeFormatado + ')';
-            listaMoedas.appendChild(option);
-        });
-    })
-    
-    xhr.send();
-}
-
-let adicionarMoedas = document.getElementById('idGerar');
-adicionarMoedas.addEventListener('click', function (e) { //Como eu quero que essa função seja executada ao carregar a pagina, posso adicionar onload? Ou DOMContentLoad?
-    e.preventDefault();
-    console.log('Passou aqui')
-    buscarMoedas(); //
-});
-
-*/
